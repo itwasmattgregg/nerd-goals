32acb631a2f4987bd90aa7d8bd739f4986bb2256
Matt Gregg <mattdgregg@gmail.com>
2021-01-13 15:59:04 -0600

    Progress bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,23 @@
 import { Provider } from "next-auth/client";
 import { AppProps } from "next/app";
+import NProgress from "nprogress";
+import Router from "next/router";
 
 import "../styles/global.css";
+import "nprogress/nprogress.css";
+
+NProgress.configure({
+  minimum: 0.3,
+  easing: "ease",
+  speed: 800,
+  showSpinner: false,
+});
+
+Router.events.on("routeChangeStart", () => {
+  NProgress.start();
+});
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
