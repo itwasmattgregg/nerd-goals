07f48e3fcad7efcadd0b3d38b44fe8e601c7636d
Matt Gregg <mattdgregg@gmail.com>
2021-01-23 16:44:37 -0600

    Fix number id

--- pages/api/user/[id].ts
+++ pages/api/user/[id].ts
@@ -4,13 +4,13 @@ import { getSession } from "next-auth/client";
 // PUT /api/user/:id
 export default async function handle(req, res) {
   const { name } = req.body;
-  const userId = req.query.id;
+  const userId: string = req.query.id;
 
   const session = await getSession({ req });
   // Check if session id is same as user id
 
   const post = await prisma.user.update({
-    where: { id: Number(userId) },
+    where: { id: userId },
     data: {
       name,
     },
